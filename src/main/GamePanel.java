88266c70bc4d2a38471012ff9627347380f8642c
Cao Duy The Anh <anh.cdt235637@sis.hust.edu.vn>
2025-05-07 18:47:32 +0700

    V1. Starting the Game Clock

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,6 @@
 package main;
 import javax.swing.JPanel;
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable {
 
     // SCRREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
@@ -12,10 +12,21 @@ public class GamePanel extends JPanel{
     public final int screenWidth = tileSize * maxScreenCol; // 768 pixels wide
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels tall
 
+    Thread gameThread; // Thread for the game loop
+
     public GamePanel() {
         this.setPreferredSize(new java.awt.Dimension(screenWidth, screenHeight));
         this.setBackground(java.awt.Color.BLACK);
         this.setDoubleBuffered(true); // Enable double buffering for smoother graphics
     }
 
+    public void startGameThread() {
+        gameThread = new Thread(this); // Create a new thread for the game loop
+        gameThread.start(); // Start the thread
+    }
+
+    @Override
+    public void run() {
+    }
+    
 }
