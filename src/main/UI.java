231bd4ac7305a6d63ea44d9402930cdb4c6b55f3
Cao Duy The Anh <anh.cdt235637@sis.hust.edu.vn>
2025-05-15 19:14:24 +0700

    V10. Display a key image

--- src/main/UI.java
+++ src/main/UI.java
@@ -4,17 +4,21 @@ public class UI {
     
     GamePanel gp;
     java.awt.Font arial_40;
+    java.awt.image.BufferedImage keyImage;
 
     public UI(GamePanel gp) {
         this.gp = gp;   
         arial_40 = new java.awt.Font("Arial", java.awt.Font.PLAIN, 40);
+        object.OBJ_Key key = new object.OBJ_Key();
+        keyImage = key.image;
     }
 
     public void draw(java.awt.Graphics2D g2) {
 
         g2.setFont(arial_40);
         g2.setColor(java.awt.Color.WHITE);
-        g2.drawString("Key = " + gp.player.hasKey, 50, 50);
+        g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+        g2.drawString("x " + gp.player.hasKey, 74, 65);
 
     }
 
