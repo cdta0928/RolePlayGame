caf2e4c6c45c2c2a1d5a6234773c1c472a2c3235
Cao Duy The Anh <anh.cdt235637@sis.hust.edu.vn>
2025-05-15 18:39:52 +0700

    V8. Intersects method

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -77,18 +77,34 @@ public class CollisionChecker {
                 switch (entity.direction) {
                     case "up":
                         entity.solidArea.y -= entity.speed;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            System.out.println("Up collision");
+                        }
                         break;
                     case "down":
                         entity.solidArea.y += entity.speed;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            System.out.println("Down collision");
+                        }
                         break;
                     case "left":
                         entity.solidArea.x -= entity.speed;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            System.out.println("Left collision");
+                        }
                         break;
                     case "right":
                         entity.solidArea.y += entity.speed;
+                        if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+                            System.out.println("Right collision");
+                        }
                         break;
                 }
             }
+            entity.solidArea.x = entity.solidAreaDefaultX;
+            entity.solidArea.y = entity.solidAreaDefaultY;
+            gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
+            gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
         }
         return index;
 
