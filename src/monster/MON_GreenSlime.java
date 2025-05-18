360c40091e97f9981c7d0373890a9f95a45609d8
Cao Duy The Anh <anh.cdt235637@sis.hust.edu.vn>
2025-05-18 17:36:13 +0700

    V24. Load slime image

--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -13,4 +13,21 @@ public class MON_GreenSlime extends entity.Entity {
         solidAreaDefaultY = 18;
     }
 
+    public void getSlimeImage() {
+
+        try {
+            up1 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_1.png"));
+            up2 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_2.png"));
+            down1 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_1.png"));
+            down2 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_2.png"));
+            left1 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_1.png"));
+            left2 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_2.png"));
+            right1 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_1.png"));
+            right2 = javax.imageio.ImageIO.read(getClass().getResourceAsStream("/res/monster/greenslime_down_2.png"));
+        } catch (java.io.IOException e) {
+            e.printStackTrace(); // Print stack trace if image loading fails
+        }
+
+    }
+
 }
