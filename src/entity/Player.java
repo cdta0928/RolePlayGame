19c5a16ae80628fc3eeee2da0c65e39b4fb09a58
Cao Duy The Anh <anh.cdt235637@sis.hust.edu.vn>
2025-05-21 13:24:31 +0700

    V32. Add axe animation

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -86,6 +86,7 @@ public class Player extends Entity {
             if (selectedItem.type == type_sword || selectedItem.type == type_axe) {
                 currentWeapon = selectedItem;
                 attack = getAttack();
+                getPlayerImage();
             }
             if (selectedItem.type == type_shield) {
                 currentShield = selectedItem;
@@ -116,14 +117,26 @@ public class Player extends Entity {
             right1 = setup("/res/player/boy_right_1", gp.tileSize, gp.tileSize);
             right2 = setup("/res/player/boy_right_2", gp.tileSize, gp.tileSize);
 
-            attackUp1 = setup("/res/player/attack/boy_attack_up_1", gp.tileSize, gp.tileSize*2);
-            attackUp2 = setup("/res/player/attack/boy_attack_up_2", gp.tileSize, gp.tileSize*2);
-            attackDown1 = setup("/res/player/attack/boy_attack_down_1", gp.tileSize, gp.tileSize*2);
-            attackDown2 = setup("/res/player/attack/boy_attack_down_2", gp.tileSize, gp.tileSize*2);
-            attackLeft1 = setup("/res/player/attack/boy_attack_left_1", gp.tileSize*2, gp.tileSize);
-            attackLeft2 = setup("/res/player/attack/boy_attack_left_2", gp.tileSize*2, gp.tileSize);
-            attackRight1 = setup("/res/player/attack/boy_attack_right_1", gp.tileSize*2, gp.tileSize);
-            attackRight2 = setup("/res/player/attack/boy_attack_right_2", gp.tileSize*2, gp.tileSize);
+            if (currentWeapon.type == type_sword) {
+                attackUp1 = setup("/res/player/attack/boy_attack_up_1", gp.tileSize, gp.tileSize*2);
+                attackUp2 = setup("/res/player/attack/boy_attack_up_2", gp.tileSize, gp.tileSize*2);
+                attackDown1 = setup("/res/player/attack/boy_attack_down_1", gp.tileSize, gp.tileSize*2);
+                attackDown2 = setup("/res/player/attack/boy_attack_down_2", gp.tileSize, gp.tileSize*2);
+                attackLeft1 = setup("/res/player/attack/boy_attack_left_1", gp.tileSize*2, gp.tileSize);
+                attackLeft2 = setup("/res/player/attack/boy_attack_left_2", gp.tileSize*2, gp.tileSize);
+                attackRight1 = setup("/res/player/attack/boy_attack_right_1", gp.tileSize*2, gp.tileSize);
+                attackRight2 = setup("/res/player/attack/boy_attack_right_2", gp.tileSize*2, gp.tileSize);
+            }
+            else if (currentWeapon.type == type_axe) {
+                attackUp1 = setup("/res/player/attack/boy_axe_up_1", gp.tileSize, gp.tileSize*2);
+                attackUp2 = setup("/res/player/attack/boy_axe_up_2", gp.tileSize, gp.tileSize*2);
+                attackDown1 = setup("/res/player/attack/boy_axe_down_1", gp.tileSize, gp.tileSize*2);
+                attackDown2 = setup("/res/player/attack/boy_axe_down_2", gp.tileSize, gp.tileSize*2);
+                attackLeft1 = setup("/res/player/attack/boy_axe_left_1", gp.tileSize*2, gp.tileSize);
+                attackLeft2 = setup("/res/player/attack/boy_axe_left_2", gp.tileSize*2, gp.tileSize);
+                attackRight1 = setup("/res/player/attack/boy_axe_right_1", gp.tileSize*2, gp.tileSize);
+                attackRight2 = setup("/res/player/attack/boy_axe_right_2", gp.tileSize*2, gp.tileSize);
+            }
     }
 
     public void update() {
