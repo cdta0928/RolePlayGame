20f202b2fc25b041524c436bc9ab6b5e20bfc1f9
Cao Duy The Anh <anh.cdt235637@sis.hust.edu.vn>
2025-05-22 20:39:54 +0700

    V37. Disable Half Transparent Effect

--- src/tile_interactive/Interactive.java
+++ src/tile_interactive/Interactive.java
@@ -28,4 +28,15 @@ public class Interactive extends entity.Entity {
             }
         }
     }
+    public void draw(java.awt.Graphics2D g2) {
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+        worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+        worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+        worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+            g2.drawImage(down1, screenX, screenY, null);
+        }
+    }
 }
